3e878fba3b4525f70d51bbdfdaf9240706c9fae1
StormiHUN <stormihun@gmail.com>
2024-03-22 14:13:54 +0100

    Movements

--- src/main/java/com/example/pong/HelloController.java
+++ src/main/java/com/example/pong/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.pong;
 
+import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -24,6 +25,51 @@ public class HelloController {
     public ToggleGroup mode;
 
 
+    int lx = 500;
+    int ly = 350;
+    int ls = 10;
+    int dx = 0;
+    int dy = ls;
+
+    int sy = 350;
+    int sp = 0;
+    int cy = 350;
+    int cp = 0;
+
+    boolean jatek = true;
+
+    AnimationTimer timer = null;
+
+    public void initialize(){
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                if(jatek) pattog();
+            }
+        };
+        timer.start();
+    }
+
+    public void pattog(){
+        if(lx+dx <= 60 && ly >= sy-70 && ly <= sy+70){
+            sp++;
+            dx = +ls;
+        }if(lx+dx >= 940 && ly >= cy-70 &&ly <= cy+70){
+            dx = -ls;
+            cp++;
+        }
+        if(ly+dy <= 30) {
+            dy = +ls;
+        }if(ly+dy >= 670) dy = -ls;
+        lx+=dx;
+        ly+=dy;
+        playerScore.setText(sp+"");
+        clientScore.setText(cp+"");
+        ball.setLayoutX(lx);
+        ball.setLayoutY(ly);
+        if(lx < 30 || lx > 970) jatek = false;
+    }
+
     public void onAcceptClick() {
 
     }
@@ -40,8 +86,10 @@ public class HelloController {
         int my = (int)e.getY(); if (my < 90) my = 90; else if(my > 610) my = 610;
         if(serverCheck.isSelected()){
             playerDisplayName.setLayoutY(my);
+            sy = my;
         }else{
             clientDisplayName.setLayoutY(my);
+            cy = my;
         }
     }
 }
